b20358526c9e67fa373361a1d9c30c6fa7e7c261
Jack Grigg <thestr4d@gmail.com>
2019-02-03 13:48:58 +0000

    Enable compiling and testing on Java 7

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,34 @@ tasks.register<Javadoc>("internalDocs") {
     options.header("")
         .addBooleanOption("private", true)
 }
+
+// Set up Java override if configured (used to test with Java 7).
+val javaHome: String by project
+val targetJavaHome = if (hasProperty("javaHome")) javaHome else System.getenv("TARGET_JAVA_HOME")
+if (targetJavaHome != null) {
+    val javaExecutablesPath = File(targetJavaHome, "bin")
+    fun javaExecutable(execName: String): String {
+        val executable = File(javaExecutablesPath, execName)
+        require(executable.exists()) { "There is no ${execName} executable in ${javaExecutablesPath}" }
+        return executable.toString()
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.apply {
+            isFork = true
+            forkOptions.javaHome = file(targetJavaHome)
+        }
+    }
+
+    tasks.withType<Javadoc>().configureEach {
+        executable = javaExecutable("javadoc")
+    }
+
+    tasks.withType<Test>().configureEach {
+        executable = javaExecutable("java")
+    }
+
+    tasks.withType<JavaExec>().configureEach {
+        executable = javaExecutable("java")
+    }
+}
