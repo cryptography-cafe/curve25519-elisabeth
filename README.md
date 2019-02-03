fe363267f5f7cdda5b6445f74ecc6304a8651eb8
Jack Grigg <thestr4d@gmail.com>
2019-02-03 13:23:27 +0000

    Gradle task for building internal documentation

--- README.md
+++ README.md
@@ -5,13 +5,23 @@ Requires Java 7 or higher.
 
 # Documentation
 
-To view the documentation, first build it:
+To view the public-facing API documentation, first build it:
 
 ```sh
 ./gradlew javadoc
 ```
 
 Then open `build/docs/javadoc/index.html` in your browser.
+
+## Internal documentation
+
+The unstable internal implementation details are also documented. To build them:
+
+```sh
+./gradlew internalDocs
+```
+
+Then open `build/docs/internal/index.html` in your browser.
 
 # About
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -14,3 +14,16 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
 }
+
+val docsDir: File by project
+
+tasks.register<Javadoc>("internalDocs") {
+    source = sourceSets["main"].allJava
+    destinationDir = file("${docsDir}/internal")
+
+    // "options" itself is the MinimalJavadocOptions interface.
+    // For some reason this is the only way to access the
+    // StandardJavadocDocletOptions backend.
+    options.header("")
+        .addBooleanOption("private", true)
+}
