241e712aba5f97c3e3263282994ce92c0df6db41
Jack Grigg <thestr4d@gmail.com>
2019-02-03 17:41:52 +0000

    Base constant-time operations
    These will eventually be moved into a separate library, but let's keep
    them here for now while things are being rewritten.

---
+++ src/main/java/cafe/cryptography/subtle/ConstantTime.java
@@ -0,0 +1,59 @@
+package cafe.cryptography.subtle;
+
+/**
+ * Constant-time functions.
+ */
+public final class ConstantTime {
+    /**
+     * Constant-time byte comparison.
+     *
+     * @param b a byte, represented as an int
+     * @param c a byte, represented as an int
+     * @return 1 if b and c are equal, 0 otherwise.
+     */
+    public static int equal(int b, int c) {
+        int result = 0;
+        int xor = b ^ c;
+        for (int i = 0; i < 8; i++) {
+            result |= xor >> i;
+        }
+        return (result ^ 0x01) & 0x01;
+    }
+
+    /**
+     * Constant-time byte[] comparison.
+     *
+     * @param b a byte[]
+     * @param c a byte[]
+     * @return 1 if b and c are equal, 0 otherwise.
+     */
+    public static int equal(byte[] b, byte[] c) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result |= b[i] ^ c[i];
+        }
+
+        return equal(result, 0);
+    }
+
+    /**
+     * Constant-time determine if byte is negative.
+     *
+     * @param b the byte to check, represented as an int.
+     * @return 1 if the byte is negative, 0 otherwise.
+     */
+    public static int isNegative(int b) {
+        return (b >> 8) & 1;
+    }
+
+    /**
+     * Get the i'th bit of a byte array.
+     *
+     * @param h the byte array.
+     * @param i the bit index.
+     * @return 0 or 1, the value of the i'th bit in h
+     */
+    public static int bit(byte[] h, int i) {
+        return (h[i >> 3] >> (i & 7)) & 1;
+    }
+}
---
+++ src/test/java/cafe/cryptography/subtle/ConstantTimeTest.java
@@ -0,0 +1,73 @@
+package cafe.cryptography.subtle;
+
+import java.util.Random;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConstantTimeTest {
+    @Test
+    public void equalOnByte() {
+        assertThat(ConstantTime.equal(0, 0), is(1));
+        assertThat(ConstantTime.equal(1, 1), is(1));
+        assertThat(ConstantTime.equal(1, 0), is(0));
+        assertThat(ConstantTime.equal(1, 127), is(0));
+        assertThat(ConstantTime.equal(-127, 127), is(0));
+        assertThat(ConstantTime.equal(-42, -42), is(1));
+        assertThat(ConstantTime.equal(255, 255), is(1));
+        assertThat(ConstantTime.equal(-255, -256), is(0));
+    }
+
+    @Test
+    public void equalOnByteArraysWithSingleDifference() {
+        byte[] zero = new byte[32];
+        byte[] one = new byte[32];
+        one[0] = 1;
+
+        assertThat(ConstantTime.equal(zero, zero), is(1));
+        assertThat(ConstantTime.equal(one, one), is(1));
+        assertThat(ConstantTime.equal(one, zero), is(0));
+        assertThat(ConstantTime.equal(zero, one), is(0));
+    }
+
+    @Test
+    public void equalOnByteArraysWithRandomData() {
+        Random random = new Random(758094325);
+
+        for (int i = 32; i < 33; i++) {
+            byte[] a = new byte[i];
+            byte[] b = new byte[i];
+
+            random.nextBytes(a);
+            random.nextBytes(b);
+
+            assertThat(ConstantTime.equal(a, a), is(1));
+            assertThat(ConstantTime.equal(b, b), is(1));
+            assertThat(ConstantTime.equal(a, b), is(0));
+            assertThat(ConstantTime.equal(b, a), is(0));
+        }
+    }
+
+    @Test
+    public void isNegative() {
+        assertThat(ConstantTime.isNegative(0), is(0));
+        assertThat(ConstantTime.isNegative(1), is(0));
+        assertThat(ConstantTime.isNegative(-1), is(1));
+        assertThat(ConstantTime.isNegative(32), is(0));
+        assertThat(ConstantTime.isNegative(-100), is(1));
+        assertThat(ConstantTime.isNegative(127), is(0));
+        assertThat(ConstantTime.isNegative(-255), is(1));
+    }
+
+    @Test
+    public void bit() {
+        assertThat(ConstantTime.bit(new byte[] { 0 }, 0), is(0));
+        assertThat(ConstantTime.bit(new byte[] { 8 }, 3), is(1));
+        assertThat(ConstantTime.bit(new byte[] { 1, 2, 3 }, 9), is(1));
+        assertThat(ConstantTime.bit(new byte[] { 1, 2, 3 }, 15), is(0));
+        assertThat(ConstantTime.bit(new byte[] { 1, 2, 3 }, 16), is(1));
+    }
+}
