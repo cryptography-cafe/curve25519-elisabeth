d3c7187bd8bfd1f21b9fa0b029e77590caae5e6f
Jack Grigg <thestr4d@gmail.com>
2019-02-05 11:20:23 +0000

    Test that x-coordinate sign is handled correctly on decompression
    Test case taken from curve25519-dalek.

--- src/test/java/cafe/cryptography/curve25519/EdwardsPointTest.java
+++ src/test/java/cafe/cryptography/curve25519/EdwardsPointTest.java
@@ -7,6 +7,12 @@ import static org.junit.Assert.assertThat;
 
 public class EdwardsPointTest {
     /**
+     * Compressed Edwards Y form of the Ed25519 basepoint.
+     */
+    static final CompressedEdwardsY ED25519_BASEPOINT_COMPRESSED = new CompressedEdwardsY(
+            Utils.hexToBytes("5866666666666666666666666666666666666666666666666666666666666666"));
+
+    /**
      * Compressed Edwards Y form of 2*basepoint.
      */
     static final CompressedEdwardsY BASE2_CMPRSSD = new CompressedEdwardsY(
@@ -14,10 +20,22 @@ public class EdwardsPointTest {
 
     @Test
     public void basepointDecompressionCompression() {
-        CompressedEdwardsY encoded = new CompressedEdwardsY(
-                Utils.hexToBytes("5866666666666666666666666666666666666666666666666666666666666666"));
-        EdwardsPoint B = encoded.decompress();
-        assertThat(B.compress(), is(encoded));
+        EdwardsPoint B = ED25519_BASEPOINT_COMPRESSED.decompress();
+        assertThat(B.compress(), is(ED25519_BASEPOINT_COMPRESSED));
+    }
+
+    @Test
+    public void decompressionSignHandling() {
+        // Manually set the high bit of the last byte to flip the sign
+        byte[] minusBasepointBytes = ED25519_BASEPOINT_COMPRESSED.toByteArray();
+        minusBasepointBytes[31] |= 1 << 7;
+        EdwardsPoint minusB = new CompressedEdwardsY(minusBasepointBytes).decompress();
+        // Test projective coordinates exactly since we know they should
+        // only differ by a flipped sign.
+        assertThat(minusB.X, is(Constants.ED25519_BASEPOINT.X.negate()));
+        assertThat(minusB.Y, is(Constants.ED25519_BASEPOINT.Y));
+        assertThat(minusB.Z, is(Constants.ED25519_BASEPOINT.Z));
+        assertThat(minusB.T, is(Constants.ED25519_BASEPOINT.T.negate()));
     }
 
     @Test
