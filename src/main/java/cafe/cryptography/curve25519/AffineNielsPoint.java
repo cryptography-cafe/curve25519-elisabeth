c985cde06dad03240acb1ebae1a2b2a919bdd300
Jack Grigg <thestr4d@gmail.com>
2019-02-06 18:29:01 +0000

    Fixed-base scalar multiplication

--- src/main/java/cafe/cryptography/curve25519/AffineNielsPoint.java
+++ src/main/java/cafe/cryptography/curve25519/AffineNielsPoint.java
@@ -1,10 +1,15 @@
 package cafe.cryptography.curve25519;
 
+import cafe.cryptography.subtle.ConstantTime;
+
 /**
  * A pre-computed point on the affine model of the curve, represented as $(y+x,
  * y-x, 2dxy)$ in "Niels coordinates".
  */
 class AffineNielsPoint {
+    static final AffineNielsPoint IDENTITY = new AffineNielsPoint(FieldElement.ONE, FieldElement.ONE,
+            FieldElement.ZERO);
+
     final FieldElement yPlusx;
     final FieldElement yMinusx;
     final FieldElement xy2D;
@@ -13,5 +18,77 @@ class AffineNielsPoint {
         this.yPlusx = yPlusx;
         this.yMinusx = yMinusx;
         this.xy2D = xy2D;
+    }
+
+    /**
+     * Constant-time selection between two AffineNielsPoints.
+     *
+     * @param that the other point.
+     * @param b    must be 0 or 1, otherwise results are undefined.
+     * @return a copy of this if $b == 0$, or a copy of that if $b == 1$.
+     */
+    public AffineNielsPoint ctSelect(AffineNielsPoint that, int b) {
+        return new AffineNielsPoint(this.yPlusx.ctSelect(that.yPlusx, b), this.yMinusx.ctSelect(that.yMinusx, b),
+                this.xy2D.ctSelect(that.xy2D, b));
+    }
+
+    /**
+     * Point negation.
+     *
+     * @return $-P$
+     */
+    public AffineNielsPoint negate() {
+        return new AffineNielsPoint(this.yMinusx, this.yPlusx, this.xy2D.negate());
+    }
+
+    /**
+     * Construct a lookup table of $[P, [2]P, [3]P, [4]P, [5]P, [6]P, [7]P, [8]P]$.
+     *
+     * @param P the point to calculate multiples for.
+     * @return the lookup table.
+     */
+    static LookupTable buildLookupTable(EdwardsPoint P) {
+        AffineNielsPoint[] points = new AffineNielsPoint[8];
+        points[0] = P.toAffineNiels();
+        for (int i = 0; i < 7; i++) {
+            points[i + 1] = P.add(points[i]).toExtended().toAffineNiels();
+        }
+        return new AffineNielsPoint.LookupTable(points);
+    }
+
+    static class LookupTable {
+        private final AffineNielsPoint[] table;
+
+        LookupTable(AffineNielsPoint[] table) {
+            this.table = table;
+        }
+
+        /**
+         * Given $-8 \leq x \leq 8$, return $[x]P$ in constant time.
+         *
+         * @param x the index.
+         * @return the pre-computed point.
+         */
+        AffineNielsPoint select(final int x) {
+            if (x < -8 || x > 8) {
+                throw new IllegalArgumentException("x is not in range -8 <= x <= 8");
+            }
+
+            // Is x negative?
+            final int xNegative = ConstantTime.isNegative(x);
+            // |x|
+            final int xabs = x - (((-xNegative) & x) << 1);
+
+            // |x| P
+            AffineNielsPoint t = AffineNielsPoint.IDENTITY;
+            for (int i = 1; i < 9; i++) {
+                t = t.ctSelect(this.table[i - 1], ConstantTime.equal(xabs, i));
+            }
+
+            // -|x| P
+            final AffineNielsPoint tminus = t.negate();
+            // [x]P
+            return t.ctSelect(tminus, xNegative);
+        }
     }
 }
---
+++ src/main/java/cafe/cryptography/curve25519/EdwardsBasepointTable.java
@@ -0,0 +1,47 @@
+package cafe.cryptography.curve25519;
+
+/**
+ * A pre-computed table of multiples of a basepoint, for accelerating fixed-base
+ * scalar multiplication.
+ */
+public class EdwardsBasepointTable {
+    final AffineNielsPoint.LookupTable[] tables;
+
+    /**
+     * Create a table of pre-computed multiples of basepoint.
+     */
+    public EdwardsBasepointTable(final EdwardsPoint basepoint) {
+        this.tables = new AffineNielsPoint.LookupTable[32];
+        EdwardsPoint Bi = basepoint;
+        for (int i = 0; i < 32; i++) {
+            this.tables[i] = AffineNielsPoint.buildLookupTable(Bi);
+            // Only every second summand is precomputed (16^2 = 256)
+            Bi = Bi.multiplyByPow2(8);
+        }
+    }
+
+    /**
+     * Constant-time fixed-base scalar multiplication.
+     *
+     * @param s the Scalar to multiply by.
+     * @return $[s]B$
+     */
+    public EdwardsPoint multiply(final Scalar s) {
+        int i;
+
+        final byte[] e = s.toRadix16();
+
+        EdwardsPoint h = EdwardsPoint.IDENTITY;
+        for (i = 1; i < 64; i += 2) {
+            h = h.add(this.tables[i / 2].select(e[i])).toExtended();
+        }
+
+        h = h.multiplyByPow2(4);
+
+        for (i = 0; i < 64; i += 2) {
+            h = h.add(this.tables[i / 2].select(e[i])).toExtended();
+        }
+
+        return h;
+    }
+}
