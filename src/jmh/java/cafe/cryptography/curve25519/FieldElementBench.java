7120cb2b2df2fa0b6da1c5ed9a7a330b430c8ac7
Jack Grigg <thestr4d@gmail.com>
2019-02-07 01:18:49 +0000

    Benchmarks for FieldElement and EdwardsPoint

---
+++ src/jmh/java/cafe/cryptography/curve25519/FieldElementBench.java
@@ -0,0 +1,62 @@
+package cafe.cryptography.curve25519;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Benchmark)
+public class FieldElementBench {
+    public FieldElement a;
+    public FieldElement b;
+
+    @Setup
+    public void prepare() {
+        byte[] a = new byte[32];
+        byte[] b = new byte[32];
+        Random r = new Random();
+        r.nextBytes(a);
+        r.nextBytes(b);
+        this.a = FieldElement.fromByteArray(a);
+        this.b = FieldElement.fromByteArray(b);
+    }
+
+    @Benchmark
+    public FieldElement add() {
+        return this.a.add(this.b);
+    }
+
+    @Benchmark
+    public FieldElement multiply() {
+        return this.a.multiply(this.b);
+    }
+
+    @Benchmark
+    public FieldElement square() {
+        return this.a.square();
+    }
+
+    @Benchmark
+    public FieldElement squareAndDouble() {
+        return this.a.squareAndDouble();
+    }
+
+    @Benchmark
+    public FieldElement invert() {
+        return this.a.invert();
+    }
+
+    @Benchmark
+    public FieldElement pow22523() {
+        return this.a.pow22523();
+    }
+}
