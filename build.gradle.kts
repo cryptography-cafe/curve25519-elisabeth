1e4305cc14bef642e2172eb57f1a882b95e500e3
Jack Grigg <thestr4d@gmail.com>
2019-02-17 18:49:53 +0000

    Add support for Maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    `maven-publish`
     jacoco
     id("me.champeau.gradle.jmh") version "0.4.8"
 }
@@ -16,6 +17,29 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+group = "cafe.cryptography"
+version = "0.0.0"
+
+tasks.register<Jar>("sourcesJar") {
+    from(sourceSets.main.get().allJava)
+    archiveClassifier.set("sources")
+}
+
+tasks.register<Jar>("javadocJar") {
+    from(tasks.javadoc)
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
 }
 
 tasks.jacocoTestReport {
